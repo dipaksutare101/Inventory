2b71027cc8afb86e1d04f3aa5d40f154777a058b
dipak <dipaksutare007@gmail.com>
2021-04-17 23:31:53 -0700

    lkgdjlgdfdsf

---
+++ Interface/InventoryDAL.cs
@@ -0,0 +1,33 @@
+﻿using Inventory.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDAL
+{
+    public class InventoryDAL : DbContext
+    {
+        public InventoryDAL() : base("name=InventoryDAL")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Sale>().ToTable("Sale");
+            modelBuilder.Entity<Saledetail>().ToTable("Saledetail");
+            modelBuilder.Entity<Registration>().ToTable("Registration");
+            modelBuilder.Entity<City>().ToTable("city");
+            modelBuilder.Entity<Image_Table>().ToTable("Image_Table");
+        }
+        public DbSet<Sale> sales { get; set; }
+        public DbSet<Saledetail> saledetails { get; set; }
+        public DbSet<Registration> registrations { get; set; }
+        public DbSet<City> cities { get; set; }
+        public DbSet<Image_Table> Image_Tables { get; set; }
+    }
+}
---
+++ Interface/InventoryDAL.cs.bak
@@ -0,0 +1,31 @@
+﻿using Inventory.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDAL
+{
+    public class InventoryDAL : DbContext
+    {
+        public InventoryDAL() : base("name=InventoryDAL")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Sale>().ToTable("Sale");
+            modelBuilder.Entity<Saledetail>().ToTable("Saledetail");
+            modelBuilder.Entity<Registration>().ToTable("Registration");
+            modelBuilder.Entity<City>().ToTable("city");
+        }
+        public DbSet<Sale> sales { get; set; }
+        public DbSet<Saledetail> saledetails { get; set; }
+        public DbSet<Registration> registrations { get; set; }
+        public DbSet<City> cities { get; set; }
+    }
+}
